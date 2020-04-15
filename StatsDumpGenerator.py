91af940def50757ecdef556d206efda750edfaca
bartoqid <5761957+bartoqid@users.noreply.github.com>
2020-04-15 11:34:27 +1000

    Add TSF capability

--- StatsDumpGenerator.py
+++ StatsDumpGenerator.py
@@ -4,27 +4,57 @@ from ip_addr_valid import ip_addr_valid
 from ip_reach import ip_reach
 from ip_file_valid import ip_file_valid
 from create_threads import create_threads
+from generate_tsf import generate_tsf
 
-#Saving the list of ip address in in ip.txt to a variable
-ip_list = ip_file_valid()
+choice = input("Enter 1 for Stats Dump and Enter 2 for Tech Support File.\nEnter 1 or 2:")
+while (choice != '1') and (choice != '2'):
+    choice = input("Dude, come one! Enter 1 for Stats Dump and Enter 2 for Tech Support File.\nEnter 1 or 2:")
 
-#verifying the validity of each ip address in the list
-try:
-    ip_addr_valid(ip_list)
+if choice == '1':
+    # Saving the list of ip address in in ip.txt to a variable
+    ip_list = ip_file_valid()
 
-except KeyboardInterrupt:
-    print("\n\n* Program aborted by user. Exiting...\n")
-    sys.exit()
+    # verifying the validity of each ip address in the list
+    try:
+        ip_addr_valid(ip_list)
 
-#Verifying the reachablity of each IP address in the list
-try:
-    ip_reach(ip_list)
+    except KeyboardInterrupt:
+        print("\n\n* Program aborted by user. Exiting...\n")
+        sys.exit()
 
-except KeyboardInterrupt:
-    print("\n\n* Program aborted by user. Exiting...\n")
-    sys.exit()
+    # Verifying the reachablity of each IP address in the list
+    try:
+        ip_reach(ip_list)
+
+    except KeyboardInterrupt:
+        print("\n\n* Program aborted by user. Exiting...\n")
+        sys.exit()
+
+    create_threads(ip_list, generate_dump)
+
+elif choice == '2':
+    # Saving the list of ip address in in ip.txt to a variable
+    ip_list = ip_file_valid()
+
+    # verifying the validity of each ip address in the list
+    try:
+        ip_addr_valid(ip_list)
+
+    except KeyboardInterrupt:
+        print("\n\n* Program aborted by user. Exiting...\n")
+        sys.exit()
+
+    # Verifying the reachablity of each IP address in the list
+    try:
+        ip_reach(ip_list)
+
+    except KeyboardInterrupt:
+        print("\n\n* Program aborted by user. Exiting...\n")
+        sys.exit()
+
+    create_threads(ip_list, generate_tsf)
 
 #Calling thrteads creation function for one or multiple ssh connections
-create_threads(ip_list, generate_dump)
+#create_threads(ip_list, generate_dump)
 
 #End of program
